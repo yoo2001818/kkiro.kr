9bd18573ff1b389eb5241e6b604b4ca9a5a03348
Duknam Yoo <yoo2001818@gmail.com>
2017-01-04 22:21:25 +0900

    Add test server side renderer

--- src/devServer.js
+++ src/devServer.js
@@ -13,7 +13,34 @@ let webpackCompiler = webpack(webpackConfig);
 let app = express();
 app.use('/metadata', devServer(config.source));
 app.use(webpackDevMiddleware(webpackCompiler, {
-  publicPath: webpackConfig.output.publicPath
+  publicPath: webpackConfig.output.publicPath,
+  serverSideRender: true
 }));
+app.get('/', (req, res) => {
+  const assetsByChunkName = res.locals.webpackStats.toJson().assetsByChunkName;
+  const publicPath = webpackConfig.output.publicPath;
+  let assets = assetsByChunkName.main;
+  if (!Array.isArray(assets)) assets = [assets];
+  res.send(`
+<html>
+  <head>
+    <title>react-blog</title>
+    ${
+      assets
+      .filter(path => path.endsWith('.css'))
+      .map(path => `<link rel="stylesheet" href="${publicPath + path}" />`)
+    }
+  </head>
+  <body>
+    <div id="root"></div>
+    ${
+      assets
+      .filter(path => path.endsWith('.js'))
+      .map(path => `<script src="${publicPath + path}"></script>`)
+    }
+  </body>
+</html>
+  `);
+});
 
 app.listen(3000, () => console.log('Server started at port 3000'));
